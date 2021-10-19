137feac4ca86c7e482fd2652512ce28b95a3f039
Jake <jakeleemiller@hotmail.com>
2021-10-19 12:50:48 -0400

    Added emit_log.php

---
+++ emit_log.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->exchange_declare('direct_logs', 'direct', false, false, false);
+
+list($queue_name, ,) = $channel->queue_declare("");
+
+$severity = isset($argv[1]) && !empty($argv[1]) ? $argv[1] : 'info';
+
+$data = implode(' ', array_slice($argv, 2));
+
+if(empty($data)){
+    $data = "info: Hello World!";
+}
+
+$msg = new AMQPMessage($data);
+
+$channel->basic_publish($msg, 'direct_logs', $severity);
+
+echo " [x] Sent ", $severity, ': ', $data, "\n";
+
+$channel->close();
+$connection->close();
